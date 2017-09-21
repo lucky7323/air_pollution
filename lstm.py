319f528b07e516631f03cf1dfa140a980fdec849
eunho <lucky7323@postech.ac.kr>
2017-09-21 19:28:32 +0900

    implement lstm algorithm and plot the errors

---
+++ lstm.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import numpy as np
+from numpy import concatenate
+from matplotlib import pyplot
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+	n_vars = 1 if type(data) is list else data.shape[1]
+	df = pd.DataFrame(data)
+	cols, names = list(), list()
+	for i in range(n_in, 0, -1):
+		cols.append(df.shift(i))
+		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+	for i in range(0, n_out):
+		cols.append(df.shift(-i))
+		if i==0:
+			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+		else:
+			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+	agg = pd.concat(cols, axis=1)
+	agg.columns = names
+
+	if dropnan:
+		agg.dropna(inplace=True)
+	return agg
+
+dataset = pd.read_csv('pollution.csv', header=0, index_col=0)
+values = dataset.values
+
+encoder = LabelEncoder()
+values[:,4] = encoder.fit_transform(values[:,4])
+values = values.astype('float32')
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled = scaler.fit_transform(values)
+
+reframed = series_to_supervised(scaled, 1, 1)
+reframed.drop(reframed.columns[[9,10,11,12,13,14,15]], axis=1, inplace=True)
+print(reframed.head())
+
+# split into train and test sets
+values = reframed.values
+n_train_hours = 365 * 24
+train = values[:n_train_hours, :]
+test = values[n_train_hours:, :]
+train_X, train_y = train[:, :-1], train[:, -1]
+test_X, test_y = test[:, :-1], test[:, -1]
+train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+
+# design network
+model = Sequential()
+model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+model.add(Dense(1))
+model.compile(loss='mae', optimizer='adam')
+
+history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+pyplot.show()
+
+yhat = model.predict(test_X)
+test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
+
+inv_yhat = concatenate((yhat, test_X[:, 1:]), axis=1)
+inv_yhat = scaler.inverse_transform(inv_yhat)
+inv_yhat = inv_yhat[:,0]
+test_y = test_y.reshape((len(test_y), 1))
+inv_y = np.concatenate((test_y, test_X[:, 1:]), axis=1)
+inv_y = scaler.inverse_transform(inv_y)
+inv_y = inv_y[:,0]
+
+rmse = np.sqrt(mean_squared_error(inv_y, inv_yhat))
+print('TEST RMSE: %.3f' % rmse)
